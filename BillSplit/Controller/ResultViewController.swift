8cd4e2c30a6b8ac288e1c3335be8551b1e0f3caa
Yan Oliveira <oliveira.yan02@gmail.com>
2022-09-02 19:08:14 -0300

    feat: add result view controller

---
+++ BillSplit/Controller/ResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultViewController.swift
+//  BillSplit
+//
+//  Created by Yan Oliveira on 02/09/22.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
