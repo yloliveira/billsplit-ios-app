b4344866aed5d871bd13b1b96a8b71f3f602884b
Yan Oliveira <oliveira.yan02@gmail.com>
2022-09-02 12:45:32 -0300

    Initial Commit

---
+++ BillSplit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BillSplit
+//
+//  Created by Yan Oliveira on 02/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
