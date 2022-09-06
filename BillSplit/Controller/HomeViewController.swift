9e0b10bf82138c36fbd21e4b75f405401aac4888
Yan Oliveira <oliveira.yan02@gmail.com>
2022-09-06 12:08:26 -0300

    feat: add home screen actions

--- BillSplit/Controller/HomeViewController.swift
+++ BillSplit/Controller/HomeViewController.swift
@@ -8,9 +8,34 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var totalTextInput: UITextField!
+    @IBOutlet weak var tipValueLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var peopleQuantityLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func tipSliderValueChanged(_ sender: UISlider) {
+        let value = String(format: "%.0f", sender.value)
+        tipValueLabel.text = "\(value)%"
+    }
+    
+    @IBAction func peopleQuantityValueChanged(_ sender: UIStepper) {
+        peopleQuantityLabel.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let peopleQuantity: Float = Float(peopleQuantityLabel.text!)!
+        let inputText: Float = Float(totalTextInput.text!)!
+        
+        let totalWithTip = inputText + (inputText * (tipSlider.value / 100))
+        
+        let total = totalWithTip / peopleQuantity
+        print(total)
+    }
+    
 }
